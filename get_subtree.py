5b2cfa99f2726a54430565805af21fab2e1a8f04
Roland Szabo <roland.szabo753@gmail.com>
2024-04-27 13:12:12 +0200

    add get_subtree generator

--- get_subtree.py
+++ get_subtree.py
@@ -1,1 +1,21 @@
 from game import Data
+
+def get_subtree_for(tax_id):
+    db = Data()
+    children = db.get_children(tax_id)
+    for child in children:
+        yield child
+
+    for child in children:
+        yield from get_subtree_for(child)
+
+    return
+
+
+if __name__ == '__main__':
+    tax_id = 219115 # Viverrinae
+    db = Data()
+    subtree = get_subtree_for(tax_id)
+
+    for s in subtree:
+        print(f"{s}: {db.get_name(s)}")
